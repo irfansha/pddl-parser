cc95aba839f3cb70b19a7301f05b66d66a7d82a9
irfansha <irfansha536@gmail.com>
2020-06-18 18:04:58 +0200

    Started generating clauses for initial state.

--- cnf_gen.py
+++ cnf_gen.py
@@ -47,19 +47,54 @@ def constraints(domain, problem):
     clause_list.append(act)
   return clause_list
 
+def make_boolvar(tup, i):
+  var = ''
+  for ele in tup:
+    var = var + str(ele) + '_'
+    #print(ele)
+  var = var + str(i)
+  #print(var)
+  return var
+
+'''
+Generates clauses with boolean variables for initial state,
+time step is considered 1:
+'''
+def initial_state_cnf_gen(state):
+  initial_clauses = []
+  for atom in state:
+    var = make_boolvar(atom,1)
+    initial_clauses.append(var)
+  return initial_clauses
+
+def goal_state_cnf_gen(state):
+  print(state)
+  return []
+
+def act_cnf_gen(constraints_list):
+  return []
+
+def cnfgen(constraint_list):
+  initial_state = constraint_list.pop(0)
+  #print("Initial state: ", initial_state)
+  initial_clauses = initial_state_cnf_gen(initial_state)
+  print(initial_clauses)
+  goal_state = constraint_list.pop(0)
+  goal_state_cnf_gen(goal_state)
+  #print("Goal state: ", goal_state)
+
+  #print('clauses:')
+  #for clause in constraint_list:
+  #   print(clause)
+  return []
+
 if __name__ == '__main__':
   import sys, time
   #start_time = time.time()
   domain = sys.argv[1]
   problem = sys.argv[2]
-  clause_list = constraints(domain, problem)
+  constraint_list = constraints(domain, problem)
   #print('Time: ' + str(time.time() - start_time) + 's')
 
-  initial_state = clause_list.pop(0)
-  print("Initial state: ", initial_state)
-  goal_state = clause_list.pop(0)
-  print("Goal state: ", goal_state)
-
-  print('clauses:')
-  for clause in clause_list:
-     print(clause)
+  cnf = cnfgen(constraint_list)
+  print(cnf)
