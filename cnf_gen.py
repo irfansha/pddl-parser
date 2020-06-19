4e454f3ad4f03988083e88787ace848233c7b367
irfansha <irfansha536@gmail.com>
2020-06-19 11:52:07 +0200

    completed single step action clause generation.

--- cnf_gen.py
+++ cnf_gen.py
@@ -7,7 +7,7 @@ commit id: 38cc9aa48208d396f4373198ef354918f548e7de
 
 '''
 Todos:
-  - generate cnf for actions
+  - update the number of steps for actions from one to k-1
   - add exactly one constraints for moves
   - Update to include propagated boolean variable in time ("untouched" by action)
 '''
@@ -69,14 +69,14 @@ def make_nboolvar(tup, i):
 Generates clauses with boolean variables for initial state,
 time step is considered 1:
 '''
-def initial_state_cnf_gen(state):
+def initial_clause_gen(state):
   initial_clauses = []
   for atom in state:
     var = make_pboolvar(atom,1)
     initial_clauses.append(var)
   return initial_clauses
 
-def goal_state_cnf_gen(state,k):
+def goal_clause_gen(state,k):
   goal_pos = state[0]
   goal_neg = state[1]
   goal_clauses = []
@@ -88,21 +88,34 @@ def goal_state_cnf_gen(state,k):
     goal_clauses.append(var)
   return goal_clauses
 
+def act_imp_clauses_gen(constraints_list,i):
+  imp_clauses = []
+  for constraint in constraints_list:
+    name = constraint.name + "_" + make_pboolvar(constraint.parameters,i)
+    action_var =  name
+    temp_imp_clauses = []
+    for cond in constraint.positive_preconditions:
+      temp_imp_clauses.append(make_pboolvar(cond,i))
+    for cond in constraint.negative_preconditions:
+      temp_imp_clauses.append(make_nboolvar(cond,i))
+    for cond in constraint.add_effects:
+      temp_imp_clauses.append(make_pboolvar(cond,i+1))
+    for cond in constraint.del_effects:
+      temp_imp_clauses.append(make_nboolvar(cond,i+1))
+    imp_clauses.append([action_var, temp_imp_clauses])
+  return imp_clauses
 def act_cnf_gen(constraints_list):
   return []
 
-def cnfgen(constraint_list, k):
+def clause_gen(constraint_list, k):
   initial_state = constraint_list.pop(0)
-  initial_clauses = initial_state_cnf_gen(initial_state)
-  print(initial_clauses)
+  initial_clauses = initial_clause_gen(initial_state)
+  #print(initial_clauses)
   goal_state = constraint_list.pop(0)
-  goal_clauses = goal_state_cnf_gen(goal_state, k)
-  print(goal_clauses)
-
-  #print('clauses:')
-  #for clause in constraint_list:
-  #   print(clause)
-  return []
+  goal_clauses = goal_clause_gen(goal_state, k)
+  #print(goal_clauses)
+  act_imp_clauses = act_imp_clauses_gen(constraint_list, 1)
+  return [initial_clauses, goal_clauses, act_imp_clauses]
 
 if __name__ == '__main__':
   import sys, time
@@ -112,6 +125,5 @@ if __name__ == '__main__':
   k = int(sys.argv[3])
   constraint_list = constraints(domain, problem)
   #print('Time: ' + str(time.time() - start_time) + 's')
-
-  cnf = cnfgen(constraint_list,k)
-  print(cnf)
+  # initial, goal and actions clauses as a list of lists:
+  clauses = clause_gen(constraint_list,k)
